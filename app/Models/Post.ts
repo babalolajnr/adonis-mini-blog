fc095547f6889df37cc63459b1ef9a753a787df0
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-02-04 10:11:14 +0100

    add column properties

--- app/Models/Post.ts
+++ app/Models/Post.ts
@@ -1,9 +1,22 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
 
 export default class Post extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public content: string
+
+  @column()
+  public userId: number
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
