b0a55f6a6cd730f6580610155f56024219d6e978
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-02-04 10:44:44 +0100

    add store method

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,11 +1,17 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import CreatePostValidator from 'App/Validators/CreatePostValidator'
 
 export default class PostsController {
   public async index({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
-
-  public async store({}: HttpContextContract) {}
+  public async store({ request, response, auth }: HttpContextContract) {
+    const payload = await request.validate(CreatePostValidator)
+    const post = await (await auth.user?.related('post').create(payload)).save()
+    return response.json({
+      status: response.created,
+      post: post,
+    })
+  }
 
   public async show({}: HttpContextContract) {}
 
