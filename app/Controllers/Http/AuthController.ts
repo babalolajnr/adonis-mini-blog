e3853357d997b8c2959160a3c5abe4d1a410f46c
Abdulqudduus Babalola <babalolajnr@gmail.com>
2022-02-04 09:18:10 +0100

    hash user's passwords and add login method

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,15 +1,35 @@
 import User from 'App/Models/User'
 import RegisterUserValidator from 'App/Validators/RegisterUserValidator'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class AuthController {
   public async register({ request, response }) {
     const payload = await request.validate(RegisterUserValidator)
+    let hashedPassword = await Hash.make(payload.password)
 
-    const user = await (await User.create(payload)).save()
+    // destructure password and other properties from the payload
+    const { password, ...other } = payload
+
+    // combine other user properties and hashed password
+    const data = { ...other, ...{ password: hashedPassword } }
+
+    const user = await (await User.create(data)).save()
 
     return response.json({
       success: 'Registration successful',
       user: user,
     })
+  }
+
+  public async login({ auth, request, response }) {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    try {
+      const token = await auth.use('api').attempt(email, password)
+      return token
+    } catch {
+      return response.badRequest('Invalid credentials')
+    }
   }
 }
